bfe067fed25b441730c243042511f131f17895fb
Vitor Natario <natariovitor@gmail.com>
2023-12-15 15:26:27 -0300

    feat: move page to (store)

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <button className="bg-violet-600 rounded py-2 px-1 hover:brightness-50 duration-300">
+      Enviar <span className="font-bold">tessdadste</span>
+    </button>
+  )
+}
