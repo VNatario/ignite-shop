1e2875b55e2c3ff0fccb4bc0df11a0196dc3c29a
Vitor Natario <natariovitor@gmail.com>
2023-12-15 23:04:55 -0300

    config: mock data

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,13 +1,50 @@
 'use client'
-import Image from 'next/image'
-import Link from 'next/link'
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
+import { ProductCard } from '@/components/productCard'
+
+const data = [
+  {
+    id: '01',
+    image: '/shirt1.png',
+    price: 79.9,
+    title: 'Camiseta Beyond the Limits',
+    link: '',
+  },
+  {
+    id: '02',
+    image: '/shirt2.png',
+    price: 79.9,
+    title: 'Camiseta Beyond the Limits',
+    link: '',
+  },
+  {
+    id: '03',
+    image: '/shirt3.png',
+    price: 79.9,
+    title: 'Camiseta Beyond the Limits',
+    link: '',
+  },
+  {
+    id: '04',
+    image: '/shirt4.png',
+    price: 79.9,
+    title: 'Camiseta Beyond the Limits',
+    link: '',
+  },
+  {
+    id: '05',
+    image: '/shirt5.png',
+    price: 79.9,
+    title: 'Camiseta Beyond the Limits',
+    link: '',
+  },
+]
 
 export default function Home() {
   const [sliderRef] = useKeenSlider({
     slides: {
-      perView: 3,
+      perView: 2,
       spacing: 48,
     },
   })
@@ -18,131 +55,15 @@ export default function Home() {
       className="flex  w-full 
       max-w-[calc(100vw-((100vw-1180px)/2))] ml-auto min-h-[656px] keen-slider"
     >
-      <Link
-        href=""
-        className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
-      >
-        <Image
-          className="h-[480px]"
-          src="/shirt1.png"
-          alt=""
-          width={520}
-          height={520}
-          quality={100}
-          priority
-        />
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-          <strong className="text-xl">Camiseta Beyond the Limits </strong>
-          <span className="font-bold text-2xl text-product-light">
-            {(79.9).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </span>
-        </footer>
-      </Link>
-      <Link
-        href=""
-        className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg p-1 relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
-      >
-        <Image
-          className="h-[480px]"
-          src="/shirt2.png"
-          alt=""
-          width={520}
-          height={520}
-          quality={100}
-          priority
-        />
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-          <strong className="text-xl">Camiseta Beyond the Limits </strong>
-          <span className="font-bold text-2xl text-product-light">
-            {(79.9).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </span>
-        </footer>
-      </Link>
-      <Link
-        href=""
-        className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg p-1 relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
-      >
-        <Image
-          className="h-[480px]"
-          src="/shirt3.png"
-          alt=""
-          width={520}
-          height={520}
-          quality={100}
-          priority
-        />
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-          <strong className="text-xl">Camiseta Beyond the Limits </strong>
-          <span className="font-bold text-2xl text-product-light">
-            {(79.9).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </span>
-        </footer>
-      </Link>
-      <Link
-        href=""
-        className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg p-1 relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
-      >
-        <Image
-          className="h-[480px]"
-          src="/shirt4.png"
-          alt=""
-          width={520}
-          height={520}
-          quality={100}
-          priority
-        />
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-          <strong className="text-xl">Camiseta Beyond the Limits </strong>
-          <span className="font-bold text-2xl text-product-light">
-            {(79.9).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </span>
-        </footer>
-      </Link>
-      <Link
-        href=""
-        className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg p-1 relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
-      >
-        <Image
-          className="h-[480px]"
-          src="/shirt5.png"
-          alt=""
-          width={520}
-          height={520}
-          quality={100}
-          priority
-        />
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
-          <strong className="text-xl">Camiseta Beyond the Limits </strong>
-          <span className="font-bold text-2xl text-product-light">
-            {(79.9).toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </span>
-        </footer>
-      </Link>
+      {data.map((product) => {
+        return (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="keen-slider__slide"
+          />
+        )
+      })}
     </main>
   )
 }
