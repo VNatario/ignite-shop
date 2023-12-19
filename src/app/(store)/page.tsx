a1f3b5c2d4fab1006f32145b5378892f9b7caff9
Vitor Natario <natariovitor@gmail.com>
2023-12-19 19:38:56 -0300

    feat: get products to stripe

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,10 +1,32 @@
 import { Carousel } from '@/components/carousel'
+import { stripe } from '@/lib/stripe'
+import Stripe from 'stripe'
 
-export default function Home() {
+async function getProducts() {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: (price.unit_amount as number) / 100,
+    }
+  })
+
+  return products
+}
+
+export default async function Home() {
+  const products = await getProducts()
+
   return (
     <main className="flex w-full max-w-[calc(100vw-((100vw-1180px)/2))] ml-auto min-h-[656px] keen-slider">
-      {/* server component */}
-      <Carousel />
+      <Carousel products={products} />
     </main>
   )
 }
--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,47 +1,19 @@
 'use client'
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
-import { ProductCard } from '@/components/productCard'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const data = [
-  {
-    id: '01',
-    image: '/shirt1.png',
-    price: 79.9,
-    title: 'Camiseta Beyond the Limits',
-    link: '',
-  },
-  {
-    id: '02',
-    image: '/shirt2.png',
-    price: 79.9,
-    title: 'Camiseta Beyond the Limits',
-    link: '',
-  },
-  {
-    id: '03',
-    image: '/shirt3.png',
-    price: 79.9,
-    title: 'Camiseta Beyond the Limits',
-    link: '',
-  },
-  {
-    id: '04',
-    image: '/shirt4.png',
-    price: 79.9,
-    title: 'Camiseta Beyond the Limits',
-    link: '',
-  },
-  {
-    id: '05',
-    image: '/shirt5.png',
-    price: 79.9,
-    title: 'Camiseta Beyond the Limits',
-    link: '',
-  },
-]
+interface CarouselProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
 
-export function Carousel() {
+export function Carousel({ products }: CarouselProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 2,
@@ -51,13 +23,34 @@ export function Carousel() {
 
   return (
     <div ref={sliderRef} className="flex">
-      {data.map((product) => {
+      {products.map((product) => {
         return (
-          <ProductCard
+          <Link
             key={product.id}
-            product={product}
-            className="keen-slider__slide"
-          />
+            href=""
+            className="bg-gradient-to-b from-gradient-green  to-gradient-purple rounded-lg relative flex items-center justify-center flex-col object-cover group overflow-hidden keen-slider__slide"
+          >
+            <Image
+              className="h-[480px]"
+              src={product.imageUrl}
+              alt=""
+              width={520}
+              height={480}
+              quality={100}
+              priority
+            />
+            <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black bg-opacity-60 p-8 translate-y-[110%] opacity-0 ease-in-out duration-300 group-hover:opacity-100 group-hover:translate-y-0">
+              <strong className="text-xl">{product.name}</strong>{' '}
+              <span className="font-bold text-2xl text-product-light">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </span>
+            </footer>
+          </Link>
         )
       })}
     </div>
